cf3e8ab80efbe14036f107aeed2636aa8d489096
dev1 <dev@gmail.com>
2025-01-01 13:03:30 +0530

    commiting files with variables

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh #no space before and after equal
+PERSON2=Suresh
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hi $PERSON1"
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: thank you i am fine $PERSON1"
